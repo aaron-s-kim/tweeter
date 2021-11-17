1c99e6cc791545e10f6d45748a1515e67f175f2f
Aaron K <tonberryspawn@gmail.com>
2021-11-16 23:20:38 -0800

    added css styling and avatar icon for tweet container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+  console.log('DOM is ready for jQuery');
+
+  $("#tweet-text").on('input', function(event) { // ES5 function convention to use 'this' inside function scope
+    // let text = event.target.value 
+
+    let numChars = $(this).val().length
+    let remChars = 140 - numChars;
+
+    $("#tweet-text").text(numChars);
+    const counter = $(this).parentsUntil(".new-tweet").find(".counter");
+    counter.html(remChars);
+
+    counter.toggleClass('over-limit', (remChars < 0));
+
+  });
+});
