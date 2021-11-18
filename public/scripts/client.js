4a2b19544372c31d404dcd71f7d9cbf04a24d6ad
Aaron K <tonberryspawn@gmail.com>
2021-11-17 17:45:16 -0800

    added fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,46 +6,66 @@
 
 $(document).ready(function () {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // // Test / driver code (temporary). Eventually will get this from the server.
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
+
+  // $(function() {
+  //   const $button = $('#load-more-posts');
+  //   $button.on('click', function () {
+  //     console.log('Button clicked, performing ajax call...');
+      
+  //     $.ajax('more-posts.html', { method: 'GET' })
+  //     .then(function (morePostsHtml) {
+  //       console.log('Success: ', morePostsHtml);
+  //       $button.replaceWith(morePostsHtml);
+  //     });
+  //   });
+  // });
+
+  // use AJAX to fetch (GET) data from server; receive arr of tweets as JSON
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'}) // http://localhost:8080/tweets
+    .then(function(arrTweets) {
+      console.log('Success:', arrTweets);
+      renderTweets(arrTweets);
+    });
+  };
+  loadTweets();
 
   // take in arr of tweet Objs, then append each to #tweets-container
   const renderTweets = function(tweets) {
-    
-    // can use regular for loop for consistency
+    // Mentor: can use regular for loop for consistency
     jQuery.each(tweets, (key) => {
-      // console.log(tweets[key].user.name);
       const tweet = createTweetElement(tweets[key]);
       $('#tweets-container').append(tweet);
     });
+  };
 
-  }
-
-  function createTweetElement(tweetData) {
-    // timeago counts year when year-mark passed
+  const createTweetElement = function(tweetData) {
+    // timeago adds count once time-mark passes
     let $tweet = $(`
     <article class="tweet">
       <header>
@@ -69,6 +89,6 @@ $(document).ready(function () {
     return $tweet;
   };
 
+  // renderTweets(data);
 
-  renderTweets(data);
 });
